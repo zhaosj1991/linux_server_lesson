f1b1f907287c37d8a783bba6cda94fff5970c112
zhaosj1991 <zhaosj_1991@163.com>
2019-11-16 18:57:05 +0800

    上个版本代码有误，不是插排和shell，重新上传。

--- sample_code/lesson01/shell_sort.c
+++ sample_code/lesson01/shell_sort.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-
-#define ARRAY_LENGTH   18
+#include <stdlib.h>
 
 int shell_sort(int *data, int length)
 {
@@ -11,22 +10,14 @@ int shell_sort(int *data, int length)
 
     for (gap = length / 2; gap >= 1; gap /= 2)
     {        
-        for (i = 0; i < length; i++)
+        for (i = gap; i < length; i++)
         {            
-            for (j = i; j >= gap; j -= gap)
-            {                
-                if (data[j] < data[j - gap])
-                {                    
-                    temp = data[j];
-                    data[j] = data[j - gap];
-                    data[j - gap] = temp;
-                }
-                else
-                {                    
-                    break;
-                }
-                
+            temp = data[i];
+            for (j = i - gap; j >= 0 && temp < data[j]; j -= gap)
+            {
+                data[j+gap] = data[j];
             }
+            data[j+gap] = temp;
         }
     }
 
@@ -40,33 +31,42 @@ int insert_sort(int *data, int length)
 
     for (i = 0; i < length; i++)
     {
-        for ( j = i; j > 0; j--)
+        temp = data[i];
+        for (j = i-1; j >= 0 && temp < data[j]; j--)
         {
-            if (data[j] < data[j - 1])
-            {
-                temp = data[j];
-                data[j] = data[j - 1];
-                data[j - 1] = temp;
-            }
-            else
-            {
-                break;
-            }
+            data[j+1] = data[j];
         }
+        data[j+1] = temp;
     }
 
     return 0;
 }
 
+#define ARRAY_LENGTH   18
+
 int main()
 {
     int i = 0;
-    int data[ARRAY_LENGTH] = { 49, 20 , 7, 10 , 8, 32, 38, 647, 3464, 3, 13, 4, 5434, 64, 4, 13, 46, 4};
+    int data[18] = { 49, 20 , 7, 10 , 8, 32, 38, 647, 3464, 3, 13, 4, 5434, 64, 4, 13, 46, 4};
+    int *data_copy1 = (int *) malloc(ARRAY_LENGTH * sizeof(int));
+    int *data_copy2 = (int *) malloc(ARRAY_LENGTH * sizeof(int));
+    memcpy(data_copy1, data, ARRAY_LENGTH * sizeof(int));
+    memcpy(data_copy2, data, ARRAY_LENGTH * sizeof(int));
 
-    shell_sort(data, ARRAY_LENGTH);
+    insert_sort(data_copy1, ARRAY_LENGTH);
     for (i = 0; i < ARRAY_LENGTH; i++)
     {
-        printf("%d ", data[i]);
+        printf("%d ", data_copy1[i]);
     }
     printf("\n");
+
+    shell_sort(data_copy2, ARRAY_LENGTH);
+    for (i = 0; i < ARRAY_LENGTH; i++)
+    {
+        printf("%d ", data_copy2[i]);
+    }
+    printf("\n");
+
+    free(data_copy1);
+    free(data_copy2);
 }
